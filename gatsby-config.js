e3fc0ee38f8a7be1ddc7ba692f576243f06c1007
Blake Haydon <bhay0010@student.monash.edu>
2020-07-20 09:28:05 +1000

    Fix sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Monash Human Power`,
+    siteUrl: `https://monashhumanpower.org`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
