c6da51c5a284f1bd7158550ee37ed21e630053a5
Blake Haydon <bhay0010@student.monash.edu>
2020-07-19 20:12:38 +1000

    Add bootstrap

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,9 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+// Import bootstrap (For basic styling and simple layout)
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/dist/jquery.min.js'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
