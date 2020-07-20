040d43fab7cd658188eabf57f39ffd9e5be23f64
Blake Haydon <bhay0010@student.monash.edu>
2020-07-20 16:27:39 +1000

    Initial header complete

--- src/components/header.js
+++ src/components/header.js
@@ -1,50 +1,8 @@
-// import { Link } from "gatsby"
-// import PropTypes from "prop-types"
-// import React from "react"
-
-// const Header = ({ siteTitle }) => (
-//   <header
-//     style={{
-//       background: `rebeccapurple`,
-//       marginBottom: `1.45rem`,
-//     }}
-//   >
-//     <div
-//       style={{
-//         margin: `0 auto`,
-//         maxWidth: 960,
-//         padding: `1.45rem 1.0875rem`,
-//       }}
-//     >
-//       <h1 style={{ margin: 0 }}>
-//         <Link
-//           to="/"
-//           style={{
-//             color: `white`,
-//             textDecoration: `none`,
-//           }}
-//         >
-//           {siteTitle}
-//         </Link>
-//       </h1>
-//     </div>
-//   </header>
-// )
-
-// Header.propTypes = {
-//   siteTitle: PropTypes.string,
-// }
-
-// Header.defaultProps = {
-//   siteTitle: ``,
-// }
-
-// export default Header
-
-
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
+
+import Socials from './socials'
 
 const Header = () => {
   const data = useStaticQuery(graphql`
@@ -71,28 +29,33 @@ const Header = () => {
 
   return (
     <header>
-      <nav class='navbar fixed-top navbar-expand-lg navbar-dark MHP-bg MHP-header'>
-        <a className='navbar-brand logo_text' href='#'>
-          <Img
-            className='d-inline-block align-top logo_image'
-            fixed={data.file.childImageSharp.fixed}
-          />
-          MHP
-        </a>
+      <nav className='navbar fixed-top navbar-expand-lg navbar-dark MHP-bg'>
 
+        {/* MHP logo and name */}
+        <div className='MHP-nav-logo'>
+          <a className='navbar-brand' href='/' >
+            <Img
+              className='d-inline-block align-top'
+              fixed={data.file.childImageSharp.fixed}
+            />
+          MHP
+          </a>
+        </div>
+
+        {/* Button that allows for the menu toggler icon */}
         <button
           className='navbar-toggler'
           type='button'
           data-toggle='collapse'
-          data-target='#navbarText'
-          aria-controls='navbarText'
+          data-target='#navbarContent'
+          aria-controls='navbarContent'
           aria-expanded='false'
           aria-label='Toggle navigation'
         >
           <span className='navbar-toggler-icon' />
         </button>
 
-        <div className='collapse navbar-collapse' id='navbarText'>
+        <div className='collapse navbar-collapse' id='navbarContent'>
 
           {/* Page Links */}
           <ul className='navbar-nav ml-auto'>
@@ -105,8 +68,9 @@ const Header = () => {
             {navItem('Contact', '#contact')}
           </ul>
 
+
           {/* Social Media Icons */}
-          {/* <SocialSidebar /> */}
+          <Socials />
 
         </div>
       </nav>
