2fdf007b6479a1f4da88bafa79945bd92a360d95
Blake Haydon <bhay0010@student.monash.edu>
2020-07-20 16:32:36 +1000

    Add spacing to css

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className='container'>
+    <div className='container-fluid'>
       <div className='row'>
         <div className='col' style={{ display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
-
           <h1>Monash Human Power!</h1>
 
         </div>
