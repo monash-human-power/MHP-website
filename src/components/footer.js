1dccf89ab6a7102ffb19a362f34b340782aa5aa2
Blake Haydon <bhay0010@student.monash.edu>
2020-07-20 10:58:53 +1000

    Add basic footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,47 @@
 import React from "react"
 
 const Footer = () => (
-    <div
-        style={{
-            background: `rebeccapurple`,
-            marginBottom: `1.45rem`,
-        }}
-    >
+    <footer className='MHP-footer MHP-bg'>
+        <div className='container'>
+            <div className='row'>
 
-        <h1>
-            Monash Human Power {new Date().getFullYear()}
-        </h1>
+                {/* Sponsor section */}
+                <div className='col-md'>
+                    <h4>Sponsor Us</h4>
+                    <p>Get in touch with us today at
+                        <a href='mailto: monashhpt@gmail.com'> monashhpt@gmail.com</a>
+                    </p>
+                </div>
 
-    </div>
+                {/* Address section */}
+                <div className='col-md'>
+                    <h4>Say Hello</h4>
+                    <p>17 Alliance Lane, Monash University</p>
+                </div>
+
+                {/* Social text */}
+                <div className='col-md'>
+                    <h4>Join Us</h4>
+                    <p>Contact us on <a href='https://www.facebook.com/MonashHPT/'> Facebook</a> </p>
+                </div>
+
+                {/* Social icons */}
+                <div className='col-md'>
+                    {/* <SocialSidebar /> */}
+                </div>
+
+            </div>
+
+            <div className='row'>
+                {/* Col is xl as it should always collapse */}
+                <div className='col-xl pt-5'>
+                    Monash Human Power {new Date().getFullYear()}
+                </div>
+            </div>
+
+        </div>
+    </footer>
+
 )
 
 
