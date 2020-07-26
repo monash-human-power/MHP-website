c878b14d6f621e9ee326830df38a22915398e302
Blake <bhay0010@student.monash.edu>
2020-07-27 02:43:16 +1000

    Add styled-components module

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,8 @@ module.exports = {
         icon: `src/images/MHP_logo_green_black.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-styled-components`,
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
