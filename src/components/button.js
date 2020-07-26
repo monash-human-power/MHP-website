f3f48c24dc2774f01ab163df864fb9f68262e175
Blake <bhay0010@student.monash.edu>
2020-07-27 04:38:57 +1000

    Add HMP css in js

--- src/components/button.js
+++ src/components/button.js
@@ -2,8 +2,28 @@ import React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 
+const ButtonLink = styled(Link)`
+/* Always use uppercase text */
+text-transform: uppercase;
+font-weight: bold;
+
+/* Remove bootstrap border */
+border: 0px;
+
+color: var(--MHP-white) !important;
+background-color: var(--MHP-purple);
+
+&:hover {
+    /* Slightly darker purple */
+    background-color: #37279e;
+  } 
+`
+
+
 const Button = ({ children, href }) => (
-    <Link className="btn btn-block MHP-btn" to={href}>{children}</Link>
+    <>
+        <ButtonLink className="btn btn-block" to={href}> {children} </ButtonLink>
+    </>
 )
 
 // TODO: Add type checking
