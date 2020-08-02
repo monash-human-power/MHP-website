dc103e0287748824c1beb31d4572a37801a39719
Blake Haydon <bhay0010@student.monash.edu>
2020-08-02 19:20:51 +1000

    Add contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import InfoBlock from "../components/info_block";
+import Button from "../components/button";
 
 import MHP_green_crosshair from "../images/Group 4.svg";
 import MHP_bike_graphic from "../images/outter_bike.png";
@@ -62,6 +63,72 @@ const IndexPage = () => (
         href="/outreach"
         reverseOrder={true}
       />
+
+      {/* Contact form */}
+      <a name="contact" />
+
+      {/* Add y axis margin for clarity */}
+      <div className="row justify-content-md-center my-5">
+        <div className="col col-lg-6">
+          {/* Contact Us Title */}
+          <h2 className="text-center">Contact Us</h2>
+
+          <form
+            name="contact"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+          >
+            {/* This hidden type is needed for netlify to work */}
+            <input type="hidden" name="form-name" value="contact" />
+
+            {/* Name Input */}
+            <div className="form-group">
+              <label for="inputName">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputName"
+                placeholder="John Doe"
+                required
+              />
+            </div>
+
+            {/* Email Input */}
+            <div className="form-group">
+              <label for="inputEmail">Email Address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="inputEmail"
+                aria-describedby="emailHelp"
+                placeholder="John.Doe@gmail.com"
+                required
+              />
+            </div>
+
+            {/* Text Input */}
+            <div className="form-group">
+              <label for="inputText">Message</label>
+              <textarea
+                className="form-control"
+                id="inputText"
+                placeholder="I <3 MHP"
+                required
+              />
+            </div>
+
+            {/* Submit Button */}
+            <div className="row">
+              <div className="col m-0 p-0">
+                <div className="my-3 mx-5">
+                  <Button type="submit">Submit</Button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   </Layout>
 );
