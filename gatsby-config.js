415313eb46f54b579190f9943e806c188d64334f
Blake Haydon <bhay0010@student.monash.edu>
2020-08-02 21:43:32 +1000

    Add markdown files to Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,10 +24,19 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
     {
+      // Images folder
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      // Markdown folder
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown`,
+        path: `${__dirname}/src/markdown`,
       },
     },
     `gatsby-transformer-sharp`,
