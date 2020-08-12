df27570d7da355177c230537ee3a3d0748885e21
Blake Haydon <bhay0010@student.monash.edu>
2020-08-12 21:47:02 +1000

    Add nice 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,57 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import styled from "styled-components";
+import Img from "gatsby-image";
 
 import Layout from "../components/layout";
 import SubpageHeading from "../components/subpage_heading";
 import SEO from "../components/seo";
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <SubpageHeading> 404: PAGE NOT FOUND </SubpageHeading>
+const CentreImage = styled.img`
+  display: block;
+  margin: auto;
+  width: 100%;
+`;
 
-    <div className="container" style={{ height: "100vh" }}>
-      <div className="row">
-        <div className="col" style={{ textAlign: "center" }}>
-          <h2>
-            Looks like we just <i>crashed</i> 🚲
-          </h2>
+const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+    query PageNotFoundQuery {
+      file(
+        relativePath: { eq: "404.md" }
+        sourceInstanceName: { eq: "markdown" }
+      ) {
+        childMarkdownRemark {
+          frontmatter {
+            heading
+            subtext
+            image {
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  `);
+  const pageNotFoundData = data.file.childMarkdownRemark.frontmatter;
+
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <SubpageHeading> {pageNotFoundData.heading} </SubpageHeading>
+
+      <div className="container" style={{ height: "100vh" }}>
+        <div className="row">
+          <div className="col-md-12 p-3">
+            <CentreImage src={pageNotFoundData.image.publicURL} />
+          </div>
+
+          <div className="col-md-12 p-3" style={{ textAlign: "center" }}>
+            <h2>{pageNotFoundData.subtext}</h2>
+          </div>
         </div>
       </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
