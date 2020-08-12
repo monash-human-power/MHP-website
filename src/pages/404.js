6cf4062df2a3271f66839c1cf5c518e83cb3d402
Blake Haydon <bhay0010@student.monash.edu>
2020-08-12 21:13:59 +1000

    Add correct crosshair naming

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,9 +9,9 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
     <SubpageHeading> 404: PAGE NOT FOUND </SubpageHeading>
 
-    <div className="container-fluid py-5" style={{ height: "100vh" }}>
+    <div className="container" style={{ height: "100vh" }}>
       <div className="row">
-        <div className="col py-5" style={{ textAlign: "center" }}>
+        <div className="col" style={{ textAlign: "center" }}>
           <h2>
             Looks like we just <i>crashed</i> 🚲
           </h2>
