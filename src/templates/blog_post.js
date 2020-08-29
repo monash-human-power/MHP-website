d1a3ab73f968aaddcf2c8174827849fc09ae7f1c
Blake Haydon <bhay0010@student.monash.edu>
2020-08-29 16:32:46 +1000

    Add basic layout for a blog post

--- src/templates/blog_post.js
+++ src/templates/blog_post.js
@@ -1,10 +1,46 @@
 import React from "react";
 import { graphql } from "gatsby";
+import styled from "styled-components";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import SubpageHeading from "../components/subpage_heading";
+
+const Subheading = styled.p`
+  font-style: italic;
+  text-align: center;
+  text-transform: capitalize;
+`;
 
 export default ({ data }) => {
-  const post = data.markdownRemark;
-  console.log(data.markdownRemark);
-  return <h1>{post.frontmatter.title}</h1>;
+  const postData = data.markdownRemark;
+  const postFrontmatter = postData.frontmatter;
+  const postInnerHTML = postData.html;
+
+  return (
+    <Layout>
+      <SEO title={postFrontmatter.title} />
+      <SubpageHeading> {postFrontmatter.title} </SubpageHeading>
+
+      {/* Main content */}
+      <div className="container mb-5">
+        <div className="row">
+          <div className="col">
+            <Subheading> {postFrontmatter.subtitle} </Subheading>
+          </div>
+        </div>
+
+        <div className="row mt-5">
+          <div className="col">
+            <div dangerouslySetInnerHTML={{ __html: postInnerHTML }} />
+            <p>
+              {postFrontmatter.author} - {postFrontmatter.date}{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export const query = graphql`
@@ -13,6 +49,9 @@ export const query = graphql`
       html
       frontmatter {
         title
+        subtitle
+        author
+        date(formatString: "MMMM YYYY")
       }
     }
   }
