c06eaba153977fb5f2daec22d8a3bf76e00f5262
Blake Haydon <blake.a.haydon@gmail.com>
2021-07-31 17:31:27 +1000

    Add outreach markdown and photos

---
+++ src/markdown/outreach.md
@@ -0,0 +1,24 @@
+---
+heading: Outreach
+meta_page_description: ""
+blocks:
+  - heading: Hands on Education
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    image: ../images/main_outreach_1.JPG
+    id: hands_on_education
+  - heading: Fun for all
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    image: ../images/main_outreach_2.JPG
+    id: fun
+  - heading: Engaging STEM
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    image: ../images/main_outreach_3.JPG
+    id: race
+reviews:
+  - person: "STEM Teacher at Westall Secondary College"
+    quote: "The kids loved it and the others that missed out were super jealous!"
+  - person: "Blake Haydon"
+    quote: "It was such a fun thing to do really enjoyed it!"
+  - person: "Blake Haydon"
+    quote: "It was such a fun thing to do really enjoyed it!"
+---
--- src/pages/outreach.js
+++ src/pages/outreach.js
@@ -4,18 +4,89 @@ import { useStaticQuery, graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-
 import SubpageHeading from "../components/subpage_heading";
 import styled from "styled-components";
+import InfoBlock from "../components/info_block";
+import ContactForm from "../components/index/contact_form";
 
 const BikePage = () => {
+  const data = useStaticQuery(graphql`
+    query OutreachPageQuery {
+      file(
+        relativePath: { eq: "outreach.md" }
+        sourceInstanceName: { eq: "markdown" }
+      ) {
+        childMarkdownRemark {
+          frontmatter {
+            heading
+            meta_page_description
+            blocks {
+              image {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+              heading
+              description
+              id
+            }
+            reviews {
+              person
+              quote
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const outreachData = data.file.childMarkdownRemark.frontmatter;
+  const infoBlockArr = outreachData.blocks;
+  const reviewsArr = outreachData.reviews;
+
   return (
     <Layout>
-      <SEO title="Outreach" description={"FILL IN LATER"} />
-      <SubpageHeading> {"Outreach"} </SubpageHeading>
+      <SEO
+        title={outreachData.heading}
+        description={outreachData.meta_page_description}
+      />
+      <SubpageHeading> {outreachData.heading} </SubpageHeading>
 
       {/* Main content */}
-      <div className="container mb-5">Outreach</div>
+      <div className="container mb-5">
+        {/* Info Blocks */}
+        <div>
+          {infoBlockArr.map((blockData, index) => (
+            <InfoBlock
+              heading={blockData.heading}
+              description={blockData.description}
+              image={blockData.image.childImageSharp.fluid}
+              key={index}
+              // Example key would be 1 (index of the data)
+              id={blockData.id}
+              // Flips the order for every second block
+              reverseOrder={index % 2 === 0}
+            />
+          ))}
+        </div>
+
+        {/* Reviews */}
+        <div className="row">
+          {reviewsArr.map((reviewData, index) => (
+            <div className="col" key={index}>
+              <h2>{reviewData.person}</h2>
+              <h2>
+                <i>{reviewData.quote}</i>
+              </h2>
+            </div>
+          ))}
+        </div>
+
+        {/* Contact Form */}
+        <ContactForm className="my-5 py-5" />
+      </div>
     </Layout>
   );
 };
