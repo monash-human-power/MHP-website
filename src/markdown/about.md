ca64e13ccf13638d488179864f21102b2652a9b3
Akbar Fadiansyah <akbarfadi01@gmail.com>
2022-09-04 18:04:57 +1000

    Extract about info to about page

--- src/markdown/about.md
+++ src/markdown/about.md
@@ -1,4 +1,48 @@
 ---
 heading: About
 meta_page_description: About MHP
+blocks:
+  - heading: Who we are
+    description:
+      "Monash Human Power is a student-led engineering team from Monash
+      University in Melbourne, Australia. Since 2015 we have been designing,
+      manufacturing, and racing fully-faired human-powered vehicles (HPV) to
+      push the limits of human speed. "
+    buttonText: Meet the members
+    image: ../images/img_1579.jpg
+    href: /team
+    id: team
+  - heading: Our Mission
+    description: Our team is aiming to break the Australian HPV F200 land speed
+      record of 94.6km/h. This year marks our largest recruitment to date,
+      bringing our team up to 60 interdisciplinary students. We take pride in
+      developing industry-ready graduates through our collaborative design and
+      manufacturing process. We believe that HPV provides a relevant and
+      immersive framework for learning STEM and we use this framework to promote
+      sustainability in schools.
+    buttonText: See the bike
+    image: ../images/our-mission.jpeg
+    href: /bike
+    id: bike
+  - heading: The Competition
+    description: The World Human Powered Speed Challenge takes place annually at
+      Battle Mountain, Nevada. Teams from around the world gather on State Route
+      305 in a quest to break the human-powered land speed record. The current
+      world record in this competition is 144 km/h.
+    buttonText: See the Competition
+    image: ../images/battle_mountain_2020_poster4.jpg
+    href: http://www.ihpva.org/home/
+    id: race
+  - heading: Outreach
+    description:
+      Monash Human Power is proud to offer a robust schools program that
+      is tailored to the 'Science as a human endeavour' Level 9 and 10 Science
+      achievement standards of the Victorian Curriculum. We hope to inspire a
+      passion for STEM and sustainability in your students. We also run
+      workshops for schools competing in the Victorian HPV Super Series and help
+      to facilitate wind tunnel testing of their vehicles.
+    image: ../images/outreach2.png
+    id: outreach
+    buttonText: Learn More
+    href: /outreach
 ---
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import InfoBlock from "../components/info_block";
 import SubpageHeading from "../components/subpage_heading";
+import SubTeams from "../components/index/subteams";
 
 const AboutPage = () => {
   const data = useStaticQuery(graphql`
@@ -17,6 +18,28 @@ const AboutPage = () => {
           frontmatter {
             heading
             meta_page_description
+            blocks {
+              image {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+              href
+              heading
+              description
+              buttonText
+              id
+            }
+            image {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid_tracedSVG
+                  ...GatsbyImageSharpFluidLimitPresentationSize
+                }
+              }
+            }
           }
         }
       }
@@ -24,13 +47,35 @@ const AboutPage = () => {
   `);
 
   const aboutData = data.file.childMarkdownRemark.frontmatter;
+  const infoBlockArr = aboutData.blocks;
 
   return (
     <Layout>
       <SEO title="About" description={aboutData.meta_page_description} />
       <SubpageHeading> {aboutData.heading} </SubpageHeading>
       {/* Main content */}
-      <div className="container mb-5">Hello world</div>
+      <div className="container mb-5">
+        {/* Info Blocks */}
+        <div>
+          {infoBlockArr.map((blockData, index) => (
+            <InfoBlock
+              heading={blockData.heading}
+              description={blockData.description}
+              buttonText={blockData.buttonText}
+              href={blockData.href}
+              image={blockData.image.childImageSharp.fluid}
+              key={index}
+              // Example key would be 1 (index of the data)
+              id={blockData.id}
+              // Flips the order for every second block
+              reverseOrder={index % 2 === 0}
+            />
+          ))}
+        </div>
+
+        {/* Sub-Teams Section */}
+        <SubTeams className="my-5 py-5" />
+      </div>
     </Layout>
   );
 };
