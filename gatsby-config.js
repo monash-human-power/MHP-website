5c4e01d2bd602dfd5cab23cfdf6b2bb1a705a3bb
Akbar Fadiansyah <akbarfadi01@gmail.com>
2022-10-11 16:22:18 +1100

    Add inline image support in markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,22 @@ module.exports = {
         path: `${__dirname}/src/blog`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
