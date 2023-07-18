1a836ab1231eeea37d36f8dcb27c50e595116d7c
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-07-18 15:13:06 +1000

    Do not draw shadows for review cell

--- src/components/review.js
+++ src/components/review.js
@@ -1,32 +1,19 @@
 import React from "react";
-import styled from "styled-components";
 import { CenteredSection, SectionHeading } from "./content";
+import { TextCellBox } from "./text_grid";
 
-const ReviewCol = styled.div`
-  border: 1px solid black;
-
-  /* Dispose of shadow */
-  transition: 0.1s;
-
-  &:hover {
-    /* Show shadow */
-    transition: 0.3s;
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-  }
-`;
+const CELLS_PER_ROW = 2;
 
 const ReviewCell = ({ person, quote }, index) => (
-  <ReviewCol className="col-md m-2 p-3" key={index}>
+  <TextCellBox className="col-md m-2 p-3" key={index}>
     <p>
       <b>
         <i>"{quote}"</i>
       </b>
     </p>
     <p>{person}</p>
-  </ReviewCol>
+  </TextCellBox>
 );
-
-const CELLS_PER_ROW = 2;
 
 const ReviewGrid = ({ className, reviewsArray }) => {
   let numRows = Math.ceil(reviewsArray.length / CELLS_PER_ROW);
--- src/components/text_grid.js
+++ src/components/text_grid.js
@@ -9,7 +9,7 @@ const DEFAULT_CELLS_PER_ROW = 3;
 /**
  * Box component  for {@link TextCell}.
  */
-const TextCellBox = styled.div`
+export const TextCellBox = styled.div`
   border: 1px solid black;
 `;
 
@@ -18,7 +18,7 @@ const TextCellBox = styled.div`
  *
  * This should be used when the cell contains a link.
  */
-const ClickableTextLinkBox = styled.div`
+export const ClickableTextLinkBox = styled(TextCellBox)`
   // Clickable cursor look
   cursor: pointer;
 
